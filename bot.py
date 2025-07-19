50f1dfd6f55468d7b2866ea94438257150a58297
Ishwar Lal <138602811+ishwar0980@users.noreply.github.com>
2025-07-19 21:32:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pyrogram.errors import FloodWait
 
 logging.basicConfig(level=logging.ERROR)
 
-CHANNELS = [int(CHANNEL) for CHANNEL in environ.get("CHANNELS", "").split()]       
+CHANNELS = [int(CHANNEL) for CHANNEL in environ.get("CHANNELS", "-1002585868566").split()]       
 AuthChat = filters.chat(CHANNELS) if CHANNELS else (filters.group | filters.channel)         
 User     = Client(name = "AcceptUser", session_string = "BQFRNDIAXNs26A9ewjsxn9EHKSgEPiMwlE1j6jP6WJnVLSnqlLbojipBIyVyOm5I4YUcgxUcsP8W1q0C4MX6n4q8Pi4dRLp_moDpDUtE8ZSjxELUXTj8FSHorFV3HVYrgyk4pfsFX4NwkiX9DlEdXOGC2lOrrDXSQ4va1jwzyJG9hNovohMhEGgcNy1ayrbRNqTAMP0KaM4VVVsrx6dx9HyM7QmxsOzyIwiTR3XvXsr90ggVfCDwWFa9P5GTr4UIsgayv7LaZ4YytNZZZ_iunCwVuofDpAB4-W1HJZM9Jt-uxMaRlDL0ac7fnjmLdvegKJZurRXnuqlJ_7sm02nr2NulDZc_YwAAAAGeBZcgAA"
                   #environ.get("SESSION"))
